ef7c28cc5ee3cb4e1fa8c57320e810e9e24c7302
Manjula Guneratne <mguneratne3@gmail.com>
2025-03-04 06:01:09 -0500

    lecture notes added

--- js06b_txt.js
+++ js06b_txt.js
@@ -10,20 +10,81 @@
       Filename: js06b.js
  */
 
+// Check if the owner's name is entered on the card
+function validateName(){
+   let cardName = document.getElementById("cardName");
+   if (cardName.validity.valueMissing){
+      cardName.setCustomValidity("Enter your name as it appears on the card");
+   } else {
+      cardName.setCustomValidity("");
+   }
+}
 
+let subButton = document.getElementById("subButton")
 
+// Validate the payment when submit button is clicked
+subButton.addEventListener("click", validateName);
 
+// Check if a credit card has been selected
+function validateCard(){
+   let card = document.forms.payment.elements.credit[0];
+   if (card.validity.valueMissing){
+      card.setCustomValidity("Select your credit card");
+   } else {
+      card.setCustomValidity("");
+   }
+}
 
+// Check if the card number is valid
+function validateNumber(){
+   let cNum = document.getElementById("cardNumber");
+   if(cNum.validity.valueMissing){
+      cNum.setCustomValidity("Enter your card number");
+   } else if (cNum.validity.patternMismatch){
+      cNum.setCustomValidity("Enter a valid card number");
+   } else if (luhn(cNum.value) === false){
+      cNum.setCustomValidity("Enter a legitimate card number");
+   } else {
+      cNum.setCustomValidity("");
+   }
+}
 
+// Check that a month is selected for the expiration date
+function validateMonth(){
+   let month = document.getElementById("expMonth");
+   if(month.selectedIndex === 0){
+      month.setCustomValidity("Select the expiration month");
+   } else {
+      month.setCustomValidity("");
+   }
+}
 
+// Check that a year is selected for the expiration date
+function validateYear(){
+   let year = document.getElementById("expYear");
+   if (year.selectedIndex === 0){
+      year.setCustomValidity("Select the expiration year");
+   } else {
+      year.setCustomValidity("");
+   }
+}
 
+function validateCVC(){
+   // Determine which card was selected
+   let card = document.querySelector('input[name="credit"]:checked').value;
+   let cvc = document.getElementById("cvc");
 
-
-
-
-
-
-
+   // Validate the CVC value
+   if (cvc.validity.valueMissing){
+      cvc.setCustomValidity("Enter your CVC number");
+   } else if ((card == "amex") && !(/^\d{4}$/.test(cvc.value))){
+      cvc.setCustomValidity("Enter a 4-digit number");
+   } else if ((card == "amex") && !(/^\d{3}$/.test(cvc.value))){
+      cvc.setCustomValidity("Enter a 3-digit number");
+   } else {
+      cvc.setCustomValidity("");
+   }
+}
 
 
 
